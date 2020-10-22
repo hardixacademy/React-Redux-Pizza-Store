e363bac8e23ee83c9fdaec04dfce95672ba8aae0
HARDIX <gimailart@gmail.com>
2020-10-22 17:01:58 +0300

    cart

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as Button } from './Button';
 export { default as Categories } from './Categories';
 export { default as SortPopup } from './SortPopup';
 export { default as PizzaItem } from './PizzaItem';
+export { default as CartItem } from './CartItem';
 export { default as PizzaItemPlaceholder } from './PizzaItem/PizzaItemPlaceholder';
